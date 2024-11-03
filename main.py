ec063bf3d48ebaa6bfbd8379393f1972f68a61d3
niranjan-gurung <gurung5130@gmail.com>
2024-11-03 14:03:41 +0000

    rect moving down..

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
+window_width, window_height = pygame.display.get_surface().get_size()
+
+player_size = pygame.Vector2(20, 40)
 player_pos = pygame.Vector2(
-  window.get_width() / 2, 
-  window.get_height() / 2
-)
+  window_width / 2 - (player_size.x / 2), 10)
+player = pygame.Rect(player_pos, player_size)
 
 if __name__ == "__main__":
-  print("hello from local machine!")
-
   # main loop:
   while running:
     
@@ -27,18 +27,27 @@ if __name__ == "__main__":
     window.fill("purple")
 
     # game render goes here..
-    pygame.draw.circle(window, "white", player_pos, 5)
+    pygame.draw.rect(
+      window, 
+      "white", 
+      pygame.Rect(player)
+    )
+
+    player.y += 50 * dt
+
+    if player.y >= window.get_height():
+      player.y = 0
 
     # process key inputs:
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_w]:
+    #     player_pos.y -= 300 * dt
+    # if keys[pygame.K_s]:
+    #     player_pos.y += 300 * dt
+    # if keys[pygame.K_a]:
+    #     player_pos.x -= 300 * dt
+    # if keys[pygame.K_d]:
+    #     player_pos.x += 300 * dt
 
     # double buffering:
     pygame.display.flip()
